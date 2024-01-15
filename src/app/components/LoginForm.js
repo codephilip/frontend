244b084d77ba5c4a905d97c99191470f91a8f8b8
codephilip <philip@meiers.in>
2024-01-15 13:10:50 -0800

    fix login

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -2,44 +2,44 @@
 require('dotenv').config();
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import './Login.css'; // Import the CSS file for styling
+import './LoginForm.css'; // Import the CSS file for styling
 
 function Login() {
     const history = useHistory();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     
-    const userServiceUrl = process.env.REACT_APP_USER_SERVICE_URL;
-    console.log('User Service URL:', userServiceUrl);
-
     const handleLogin = async () => {
         try {
-            console.log('Attempting to log in...');
-            console.log('Username:', username);
-            console.log('Password:', password);
-            console.log('User service URL:', process.env.REACT_APP_USER_SERVICE_URL);
-
-            // Authenticate user with your microservice...
-            if (authenticationSuccessful) {
-                // Redirect to restricted access page
-                history.push('/restricted');
-            }
-
-            const response = await fetch(`${process.env.REACT_APP_USER_SERVICE_URL}auth/login`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-    
-            console.log('Response:', response);
-    
-            // Rest of the code...
+          console.log('Attempting to log in...');
+          console.log('Username:', username);
+          console.log('Password:', password);
+          console.log('User service URL:', process.env.REACT_APP_USER_SERVICE_URL);
+      
+          const response = await fetch(`${process.env.REACT_APP_USER_SERVICE_URL}auth/login`, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+          });
+      
+          if (response.ok) {
+            // Authentication was successful
+            const data = await response.json();
+            console.log('Login successful');
+            // You can store the token, perform redirects, or other actions here
+          } else {
+            // Authentication failed, handle error
+            console.error('Login failed');
+            // You can display error messages or perform other error handling
+          }
+      
+          // Rest of the code...
         } catch (error) {
-            console.error('Error logging in:', error);
+          console.error('Error logging in:', error);
         }
-    };
+      };
 
     return (
         <div className="login-form">
